32475e7204dd747c032703d4da1782f8021d1f80
Rifat <rifat@softbyteit.com>
2021-02-26 23:06:37 +0600

    --init project

---
+++ src/main/java/com/inzamamul/gateway/SpringApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.inzamamul.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiGatewayApplication.class, args);
+	}
+
+}
