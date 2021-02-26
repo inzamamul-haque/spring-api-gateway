1f3f7ab826e0e3c51477561b355d4992ae6ace2b
Rifat <rifat@softbyteit.com>
2021-02-27 02:00:49 +0600

    --Spring Gateway Server

--- src/main/java/com/inzamamul/gateway/SpringApiGatewayApplication.java
+++ src/main/java/com/inzamamul/gateway/SpringApiGatewayApplication.java
@@ -2,7 +2,9 @@ package com.inzamamul.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringApiGatewayApplication {
 
